d2f6c3da3cb7d31ca71b7975b987967e7f965ef2
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-10-13 11:06:36 +0900

    fix

--- functions/src/utils/index.ts
+++ functions/src/utils/index.ts
@@ -1,9 +1,7 @@
-export { postActivity } from './post-activity';
 export { addNotification } from './add-notification';
 export { countUp } from './count-up';
 export { countDown } from './count-down';
 export { db, TimeStamp } from './db';
 export { updateDailyProgress } from './update-daily-progress';
-export { addPointHelper } from './add-point-helper';
 export { sendEmail } from './sendgrid';
 export { sendFCM } from './fcm';
--- functions/src/utils/post-activity.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-import { config } from '../config';
-
-export const postActivity = (content: string) => {
-  return axios.post(config.webhook.discord, { content });
-};
