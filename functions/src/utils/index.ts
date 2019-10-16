4162aa365a56ae61cf584bbd8be7f428fd07f461
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-10-16 20:34:23 +0900

    fix

--- functions/src/utils/index.ts
+++ functions/src/utils/index.ts
@@ -1,7 +1,6 @@
 export { countUp } from './count-up';
 export { countDown } from './count-down';
 export { db, TimeStamp } from './db';
-export { updateDailyProgress } from './update-daily-progress';
 export { sendEmail } from './sendgrid';
 export { sendFCM } from './fcm';
 export * from './charge';
