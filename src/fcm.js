dcc31c20deef8dc9ddc6a966b117f060650d617a
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-10-19 14:52:21 +0900

    update

--- src/fcm.js
+++ src/fcm.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
 firebase.initializeApp({
-  'messagingSenderId': '11098690567'
+  'messagingSenderId': '848924725465'
 });
 
 const messaging = firebase.messaging();
