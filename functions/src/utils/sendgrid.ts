c67e5c82b13f2edd58df997eb93d2129e73f9f1a
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-11-06 13:35:00 +0900

    update

--- functions/src/utils/sendgrid.ts
+++ functions/src/utils/sendgrid.ts
@@ -14,7 +14,7 @@ export const sendEmail = (data: {
   return sgMail.send({
     from: {
       email: 'noreply@deer.co.jp',
-      name: 'ゴミナレッジ共有所'
+      name: 'ゴミナレッジ共有所（仮）'
     },
     ...data,
     templateId: config.mailTemplate[data.templateId]
