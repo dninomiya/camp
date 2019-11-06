d95a6743f85c719141d107587030c3e398f4b506
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-11-06 17:06:16 +0900

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  title: 'エンジニアのゴミ箱（仮）',
+  title: 'サービス名検討中',
   stripe: {
     publicKey: 'pk_live_wlNER2telcowyGuSx1mGTrGb',
     clientId: 'ca_G18JUfv56rvyII64gIBw9KWHzu15m44g'
