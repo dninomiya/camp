23e94753942ee2fa25831854744b9d0af78df2ca
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-02-12 00:48:38 +0900

    update

--- src/app/shared/plan.pipe.ts
+++ src/app/shared/plan.pipe.ts
@@ -4,18 +4,18 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'plan'
 })
 export class PlanPipe implements PipeTransform {
-
-  transform(plan: 'free' | 'isa' | 'standard', ...args: any[]): any {
+  transform(plan: 'free' | 'lite' | 'isa' | 'standard', ...args: any[]): any {
     switch (plan) {
       case 'free':
         return 'フリー';
       case 'isa':
         return 'ISA';
+      case 'lite':
+        return 'ライト';
       case 'standard':
         return 'スタンダード';
       default:
         return 'フリー';
     }
   }
-
 }
