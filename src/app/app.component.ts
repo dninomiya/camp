e99bfc7530e06de51888e940bd8f5e21871c2af2
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-03-26 12:37:31 +0900

    add dev icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Meta } from '@angular/platform-browser';
+import { Component, Inject } from '@angular/core';
 import { LogUpdateService } from './services/log-update.service';
 import { SeoService } from './services/seo.service';
 import { environment } from 'src/environments/environment';
@@ -7,17 +9,21 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   schema$: Observable<object> = this.seoService.schema$;
 
   constructor(
     private logUpdateService: LogUpdateService,
-    private seoService: SeoService
+    private seoService: SeoService,
+    @Inject(DOCUMENT) private _document: HTMLDocument
   ) {
     if (!environment.production) {
       this.seoService.addNoIndex();
+      this._document
+        .querySelector('[rel=icon')
+        .setAttribute('href', 'favicon-dev.svg');
     }
   }
 }
