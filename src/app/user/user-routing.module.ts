83e41c3132722bf5d836231901c5452928929d85
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-03-30 13:54:19 +0900

    update

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 
-
 const routes: Routes = [
   {
-    path: ':id',
+    path: '',
     component: UserComponent
   }
 ];
@@ -14,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class UserRoutingModule { }
+export class UserRoutingModule {}
