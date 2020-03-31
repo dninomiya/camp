e008ab423f749fab1ccf3a1c7be1c06d0e42d8e6
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-03-31 16:09:47 +0900

    refactor

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,16 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { TrendService } from 'src/app/services/trend.service';
 import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { AuthService } from 'src/app/services/auth.service';
 import { ChannelService } from 'src/app/services/channel.service';
-import { map, shareReplay, switchMap, tap } from 'rxjs/operators';
+import { map, shareReplay, switchMap } from 'rxjs/operators';
 import { SeoService } from 'src/app/services/seo.service';
-import { LoadingService } from 'src/app/services/loading.service';
 import { Title } from '@angular/platform-browser';
-import { LessonList } from 'src/app/interfaces/lesson-list';
-import { ListService } from 'src/app/services/list.service';
-import { SearchParameters } from 'angular-instantsearch/instantsearch/instantsearch';
 
 @Component({
   selector: 'app-home',
