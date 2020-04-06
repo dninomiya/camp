5a17ad2d43d6d1ac6ff769679b6583ee724cd203
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-04-06 10:23:20 +0900

    refactor

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { AuthService } from 'src/app/services/auth.service';
-import { ChannelService } from 'src/app/services/channel.service';
-import { map, shareReplay, switchMap } from 'rxjs/operators';
 import { SeoService } from 'src/app/services/seo.service';
 import { Title } from '@angular/platform-browser';
 
@@ -23,12 +19,7 @@ export class HomeComponent implements OnInit {
     filters: 'public:true AND NOT deleted:true',
   };
 
-  constructor(
-    private authService: AuthService,
-    private channelService: ChannelService,
-    private seoService: SeoService,
-    private titleService: Title
-  ) {
+  constructor(private seoService: SeoService, private titleService: Title) {
     this.seoService.setSchema({
       '@type': 'WebSite',
       logo: '/assets/images/logo.png',
