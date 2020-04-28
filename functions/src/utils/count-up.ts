150aaef6eccac6b37899aecc692a03e05bd4c7a1
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-04-28 10:50:55 +0900

    fix

--- functions/src/utils/count-up.ts
+++ functions/src/utils/count-up.ts
@@ -1,7 +1,13 @@
 import { db } from './db';
 import * as admin from 'firebase-admin';
 
-export const countUp = (path: string, key: string, value=1) => {
-  const ref = db.doc(path);
-  return ref.update(key, admin.firestore.FieldValue.increment(value));
+export const countUp = async (path: string, key: string, value = 1) => {
+  try {
+    const ref = db.doc(path);
+    return ref.update(key, admin.firestore.FieldValue.increment(value));
+  } catch (error) {
+    console.log(path);
+    console.log(error);
+    return error;
+  }
 };
