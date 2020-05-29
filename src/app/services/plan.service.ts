fe7bc5551031d13e649963bd7c94c2e3f5bc8162
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-05-30 08:28:28 +0900

    update

--- src/app/services/plan.service.ts
+++ src/app/services/plan.service.ts
@@ -2,23 +2,22 @@ import { Injectable } from '@angular/core';
 import { Plan } from '../interfaces/plan';
 import * as moment from 'moment';
 
-export const PLANS: Plan[] = [
-  {
-    id: 'lite',
+export const PLAN: {
+  [key: string]: Omit<Plan, 'id'>;
+} = {
+  lite: {
     title: 'ライト',
     subTitle: '教材閲覧のみ',
     amount: 12500,
     points: ['有料教材の閲覧', '教材に関する質問'],
   },
-  {
-    id: 'solo',
+  solo: {
     title: 'ソロ',
     subTitle: 'ひとりで学びたい人',
     amount: 30000,
     points: ['有料教材の閲覧', '無制限な質問'],
   },
-  {
-    id: 'mentor',
+  mentor: {
     title: 'メンター',
     subTitle: 'メンターと進めたい人',
     amount: 85000,
@@ -32,13 +31,22 @@ export const PLANS: Plan[] = [
       '就職支援',
     ],
   },
-];
+};
+
+export const PLANS: Plan[] = Object.entries(PLAN).map(([id, value]) => ({
+  id,
+  ...value,
+}));
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlanService {
   plans = PLANS;
+  plan = this.plans.reduce((obj, plan) => {
+    obj[plan.id] = plan;
+    return obj;
+  }, {});
   isCampaign = moment().isBefore('2020-05-01');
 
   constructor() {}
---
+++ src/app/shared/resource.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ResourceIconPipe } from './resource-icon.pipe';
+
+describe('ResourceIconPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ResourceIconPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
