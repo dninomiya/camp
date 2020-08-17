8d23e4b80814f52bf6c9c335d65474c70c08ee8d
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-08-17 16:15:40 +0900

    refactor

--- functions/src/stripe/index.ts
+++ functions/src/stripe/index.ts
@@ -3,7 +3,6 @@ export * from './charge.function';
 export * from './subscription.function';
 export * from './intent.function';
 export * from './payment-method.function';
-export * from './customer-portal.function';
 export * from './product.function';
 export * from './price.function';
 export * from './coupon.function';
